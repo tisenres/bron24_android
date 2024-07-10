befa888ab6275986d124d402377dae38822e8f32
tisenres <tisenres@gmail.com>
2024-07-10 21:11:03 +0700

    test commit

--- app/src/main/java/com/bron24/bron24_android/features/language/presentation/LanguageViewModel.kt
+++ app/src/main/java/com/bron24/bron24_android/features/language/presentation/LanguageViewModel.kt
@@ -4,7 +4,7 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import com.bron24.bron24_android.features.language.domain.model.Language
-import com.bron24.bron24_android.features.language.domain.util.LocaleManager
+import com.bron24.bron24_android.core.util.LocaleManager
 import com.bron24.bron24_android.features.language.domain.usecases.GetAvailableLanguagesUseCase
 import com.bron24.bron24_android.features.language.domain.usecases.UpdateSelectedLanguageUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
