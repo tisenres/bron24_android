f5c1f019d491585422e973b76b947fb346cb9b5c
tisenres <tisenres@gmail.com>
2024-07-19 16:49:56 +0700

    Colors and Theme updated

--- app/src/main/java/com/bron24/bron24_android/features/filter/presentetion/SearchView.kt
+++ app/src/main/java/com/bron24/bron24_android/features/filter/presentetion/SearchView.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -30,10 +31,11 @@ val gilroyFontFamily = FontFamily(
 )
 
 @Composable
-fun SearchView(modifier: Modifier = Modifier) {
+fun SearchView() {
     Box(
-        modifier = modifier
-            .size(337.97.dp, 144.57.dp)
+        modifier = Modifier
+            .height(144.57.dp)
+            .fillMaxWidth()
             .background(
                 color = Color(0xFF32B768),
                 shape = RoundedCornerShape(20.dp)
@@ -42,13 +44,15 @@ fun SearchView(modifier: Modifier = Modifier) {
     ) {
         Box(
             modifier = Modifier
-                .size(337.97.dp, 144.57.dp)
+                .fillMaxWidth()
                 .background(
-                    brush = Brush.horizontalGradient(
+                    brush = Brush.linearGradient(
                         colors = listOf(
                             Color(0xFFA4ECC3),
-                            Color(0xFF69CE91)
-                        )
+                            Color(0xFF69CE91),
+                        ),
+                        start = Offset.Zero,
+                        end = Offset(0F, 153F),
                     ),
                     shape = RoundedCornerShape(20.dp)
                 )
@@ -59,85 +63,94 @@ fun SearchView(modifier: Modifier = Modifier) {
                 verticalArrangement = Arrangement.Top,
                 modifier = Modifier.fillMaxSize()
             ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.Start,
-                    modifier = Modifier
-                        .padding(start = 18.74.dp, top = 25.36.dp)
-                        .rotate(0.25f)
-                ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.ronaldo),
-                        contentDescription = "profile_image",
-                        modifier = Modifier
-                            .size(28.dp)
-                            .clip(CircleShape)
-                    )
-                    Spacer(modifier = Modifier.width(14.92.dp))
-                    Text(
-                        text = "Salom, Cristiano Ronaldo!",
-                        style = TextStyle(
-                            fontFamily = gilroyFontFamily,
-                            fontWeight = FontWeight.Normal,
-                            fontSize = 14.sp,
-                            color = Color.White,
-                            lineHeight = 16.8.sp
-                        ),
-                    )
-                    Spacer(modifier = Modifier.width(87.3.dp))
-                    Image(
-                        painter = painterResource(id = R.drawable.ic_filter),
-                        contentDescription = "filter_icon",
-                        modifier = Modifier.size(width = 12.56.dp, height = 13.56.dp)
-                    )
-                }
-
-                Box(
-                    modifier = Modifier
-                        .padding(horizontal = 18.94.dp, vertical = 34.5.dp)
-                        .background(
-                            color = Color(0xFF7ADDA2),
-                            shape = RoundedCornerShape(5.dp)
-                        )
-                        .shadow(
-                            elevation = 10.dp,
-                            shape = RoundedCornerShape(5.dp),
-                            ambientColor = Color(0xFF14733B).copy(alpha = 0.23f),
-                            spotColor = Color(0xFF14733B).copy(alpha = 0.23f)
-                        )
-                        .rotate(0.25f)
-                        .size(width = 310.96.dp, height = 26.dp)
-                ) {
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.Start,
-                        modifier = Modifier.fillMaxWidth()
-                    ) {
-                        Image(
-                            painter = painterResource(id = R.drawable.ic_search),
-                            contentDescription = "search_icon",
-                            modifier = Modifier.size(11.dp)
-                        )
-                        Spacer(modifier = Modifier.width(8.96.dp))
-                        Text(
-                            text = "Search your stadium",
-                            style = TextStyle(
-                                fontFamily = gilroyFontFamily,
-                                fontWeight = FontWeight.Normal,
-                                fontSize = 10.sp,
-                                color = Color(0xFFC2F5D7),
-                                lineHeight = 12.sp
-                            ),
-                        )
-                    }
-                }
+                ProfileRow()
+                SearchRow()
             }
         }
     }
 }
 
 @Composable
+fun ProfileRow() {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 18.74.dp, top = 25.36.dp, end = 22.19.dp)
+            .rotate(0.25f)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ronaldo),
+                contentDescription = "profile_image",
+                modifier = Modifier
+                    .size(28.dp)
+                    .clip(CircleShape)
+            )
+            Spacer(modifier = Modifier.width(14.92.dp))
+            Text(
+                text = "Salom, Cristiano Ronaldo!",
+                style = TextStyle(
+                    fontFamily = gilroyFontFamily,
+                    fontWeight = FontWeight.Normal,
+                    fontSize = 14.sp,
+                    color = Color.White,
+                    lineHeight = 16.8.sp
+                ),
+            )
+        }
+        Image(
+            painter = painterResource(id = R.drawable.ic_filter),
+            contentDescription = "filter_icon",
+            modifier = Modifier.size(width = 12.56.dp, height = 13.56.dp)
+        )
+    }
+}
+
+@Composable
+fun SearchRow() {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Start,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 18.85.dp, top = 34.5.dp, end = 10.02.dp)
+            .background(
+                color = Color(0xFF7ADDA2),
+                shape = RoundedCornerShape(5.dp)
+            )
+            .shadow(
+                elevation = 10.dp,
+                spotColor = Color(0x3B13733A),
+            )
+            .height(26.dp)
+            .padding(horizontal = 10.dp)
+            .rotate(0.25f)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_search),
+            contentDescription = "search_icon",
+            modifier = Modifier.size(11.dp)
+        )
+        Spacer(modifier = Modifier.width(8.96.dp))
+        Text(
+            text = "Search your stadium",
+            style = TextStyle(
+                fontFamily = gilroyFontFamily,
+                fontWeight = FontWeight.Normal,
+                fontSize = 10.sp,
+                color = Color(0xFFC2F5D7),
+                lineHeight = 12.sp
+            ),
+        )
+    }
+}
+
+@Composable
 @Preview(showBackground = true)
 fun PreviewSearchView() {
-    SearchView(modifier = Modifier.padding(20.dp))
+    SearchView()
 }
