c1af5b49acae47777eaadb70b4b0118ca85c13b6
tisenres <tisenres@gmail.com>
2024-07-21 22:49:20 +0700

    AreaListing fetch data from endpoint

--- app/src/main/java/com/bron24/bron24_android/features/venuelisting/data/remote/VenueApiService.kt
+++ app/src/main/java/com/bron24/bron24_android/features/venuelisting/data/remote/VenueApiService.kt
@@ -4,6 +4,6 @@ import com.bron24.bron24_android.features.venuelisting.data.dto.VenueDto
 import retrofit2.http.GET
 
 interface VenueApiService {
-    @GET("api/v1/venues")
+    @GET("api/v1/venues/")
     suspend fun getVenues(): List<VenueDto>
 }
