fa993f736a57ecf87c8920658773fec96520ede0
tisenres <tisenres@gmail.com>
2024-07-25 19:30:27 +0700

    BottomBar

--- app/src/main/java/com/bron24/bron24_android/features/home/presentation/HomePage.kt
+++ app/src/main/java/com/bron24/bron24_android/features/home/presentation/HomePage.kt
@@ -2,12 +2,10 @@ package com.bron24.bron24_android.features.home.presentation
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.bron24.bron24_android.features.filter.presentetion.SearchView
 import com.bron24.bron24_android.features.venuelisting.presentation.VenueListingView
 
