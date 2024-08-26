3aa3f3f46d7bb03a4468dfe40d83eeee19e4c734
tisenres <tisenres@gmail.com>
2024-08-27 00:20:29 +0700

    Redo Toast Design

--- app/src/main/java/com/bron24/bron24_android/helper/util/presentation/components/toast/CustomToast.kt
+++ app/src/main/java/com/bron24/bron24_android/helper/util/presentation/components/toast/CustomToast.kt
@@ -6,70 +6,30 @@ import android.os.VibrationEffect
 import android.os.Vibrator
 import androidx.compose.animation.*
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.content.ContextCompat
+import com.bron24.bron24_android.screens.main.theme.gilroyFontFamily
 import kotlinx.coroutines.delay
-
-//@Composable
-//fun CustomToast(
-//    message: String,
-//    type: ToastType,
-//    durationMillis: Long = 3000,
-//    onDismiss: () -> Unit
-//) {
-//    var isVisible by remember { mutableStateOf(true) }
-//    val context = LocalContext.current
-//
-//    LaunchedEffect(key1 = message) {
-//        if (type == ToastType.ERROR) {
-//            vibrate(context)
-//        }
-//        delay(durationMillis)
-//        isVisible = false
-//        onDismiss()
-//    }
-//
-//    AnimatedVisibility(
-//        visible = isVisible,
-//        enter = slideInVertically(initialOffsetY = { -it }) + fadeIn(),
-//        exit = slideOutVertically(targetOffsetY = { -it }) + fadeOut()
-//    ) {
-//        Box(
-//            modifier = Modifier
-//                .fillMaxWidth()
-//                .padding(16.dp)
-//        ) {
-//            Box(
-//                modifier = Modifier
-//                    .align(Alignment.TopCenter)
-//                    .background(
-//                        color = when (type) {
-//                            ToastType.ERROR -> Color(0xFFFF6B6B)
-//                            ToastType.INFO -> Color(0xFF32B768)
-//                        },
-//                        shape = RoundedCornerShape(10.dp)
-//                    )
-//                    .padding(16.dp)
-//            ) {
-//                Text(
-//                    text = message,
-//                    color = Color.White,
-//                    fontSize = 16.sp
-//                )
-//            }
-//        }
-//    }
-//}
-//
 
 @Composable
 fun CustomToast(
@@ -102,23 +62,48 @@ fun CustomToast(
         ) {
             Box(
                 modifier = Modifier
-                    .align(Alignment.TopCenter)
-                    .background(
+                    .align(Alignment.TopStart)
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(5.dp))
+                    .background(Color.White)
+                    .border(
+                        width = 2.dp,
                         color = when (type) {
                             ToastType.SUCCESS -> Color(0xFF32B768)
                             ToastType.ERROR -> Color(0xFFFF6B6B)
                             ToastType.WARNING -> Color(0xFFFFA726)
                             ToastType.INFO -> Color(0xFF4CAF50)
                         },
-                        shape = RoundedCornerShape(10.dp)
                     )
                     .padding(16.dp)
             ) {
-                Text(
-                    text = message,
-                    color = Color.White,
-                    fontSize = 16.sp
-                )
+                Row(horizontalArrangement = Arrangement.spacedBy(6.dp)) {
+                    Icon(
+                        imageVector = when (type) {
+                            ToastType.SUCCESS -> Icons.Default.Done
+                            ToastType.ERROR -> Icons.Default.Clear
+                            ToastType.WARNING -> Icons.Default.Notifications
+                            ToastType.INFO -> Icons.Default.Info
+                        },
+                        contentDescription = "Toast icon",
+                        tint = when (type) {
+                            ToastType.SUCCESS -> Color(0xFF32B768)
+                            ToastType.ERROR -> Color(0xFFFF6B6B)
+                            ToastType.WARNING -> Color(0xFFFFA726)
+                            ToastType.INFO -> Color(0xFF4CAF50)
+                        })
+                    Text(
+                        text = message,
+                        style = TextStyle(
+                            fontFamily = gilroyFontFamily,
+                            fontWeight = FontWeight.Medium,
+                            fontSize = 18.sp,
+                            color = Color.Black,
+                            lineHeight = 20.sp,
+                        ),
+                        modifier = Modifier.align(Alignment.CenterVertically),
+                    )
+                }
             }
         }
     }
@@ -150,4 +135,10 @@ private fun vibrate(context: Context) {
             it.vibrate(200)
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BottomNavigationBarPreview() {
+    CustomToast(message = "Internet is not available", type = ToastType.ERROR, onDismiss = {})
 }
