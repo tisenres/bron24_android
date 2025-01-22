b4072fde058f4e6243484d7503ccc56ae8854999
tisenres <tisenres@gmail.com>
2025-01-22 13:37:27 +0500

    Added HttpInterceptor logging

--- app/src/main/java/com/bron24/bron24_android/di/NetworkModule.kt
+++ app/src/main/java/com/bron24/bron24_android/di/NetworkModule.kt
@@ -2,7 +2,8 @@ package com.bron24.bron24_android.di
 
 import android.content.Context
 import android.util.Log
-import com.bron24.bron24_android.data.network.interceptors.NoConnectivityException
+import com.bron24.bron24_android.BuildConfig
+import com.bron24.bron24_android.data.network.interceptors.HttpInterceptor
 import com.bron24.bron24_android.domain.repository.TokenRepository
 import com.bron24.bron24_android.domain.usecases.language.GetSelectedLanguageUseCase
 import com.bron24.bron24_android.helper.util.AuthAuthenticator
@@ -14,9 +15,9 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.io.IOException
 import javax.inject.Singleton
 
 @Module
@@ -30,10 +31,23 @@ class NetworkModule {
     @Provides
     @Singleton
     fun providesOkHttpClient(
-        chuckerInterceptor: ChuckerInterceptor
-    ): OkHttpClient = OkHttpClient.Builder()
-        .addInterceptor(chuckerInterceptor)
-        .build()
+        chuckerInterceptor: ChuckerInterceptor,
+        httpInterceptor: HttpInterceptor,
+    ): OkHttpClient {
+
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        return OkHttpClient.Builder()
+            .addInterceptor(chuckerInterceptor)
+            .addInterceptor(httpInterceptor)
+            .apply {
+                if (BuildConfig.DEBUG) {
+                    addInterceptor(logging)
+                }
+            }
+            .build()
+    }
 
     @Provides
     @Singleton
@@ -41,38 +55,46 @@ class NetworkModule {
     fun provideOkHttpClient(
         tokenRepository: TokenRepository,
         chuckerInterceptor: ChuckerInterceptor,
+        httpInterceptor: HttpInterceptor,
         authAuthenticator: AuthAuthenticator,
         getSelectedLanguageUseCase: GetSelectedLanguageUseCase
-    ): OkHttpClient = OkHttpClient.Builder()
-        .addInterceptor(chuckerInterceptor)
-        .authenticator(authAuthenticator)
-        .addNetworkInterceptor { chain ->
-            Log.d("AAA", "provideOkHttpClient: ${getSelectedLanguageUseCase.invoke().languageCode}")
-            val token = tokenRepository.getAccessToken() ?: ""
-            val lanCode = getSelectedLanguageUseCase.invoke().languageCode?:""
-            val newRequest = chain.request().newBuilder()
-            newRequest.header("Authorization", "Bearer $token")
-            newRequest.header("Accept-Language",lanCode)
-            chain.proceed(newRequest.build())
-        }
-        .build()
+    ): OkHttpClient {
+
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        return OkHttpClient.Builder()
+            .addInterceptor(chuckerInterceptor)
+            .addInterceptor(httpInterceptor)
+            .authenticator(authAuthenticator)
+            .addNetworkInterceptor { chain ->
+                Log.d("AAA", "provideOkHttpClient: ${getSelectedLanguageUseCase.invoke().languageCode}")
+                val token = tokenRepository.getAccessToken() ?: ""
+                val lanCode = getSelectedLanguageUseCase.invoke().languageCode ?: ""
+                val newRequest = chain.request().newBuilder()
+                newRequest.header("Authorization", "Bearer $token")
+                newRequest.header("Accept-Language", lanCode)
+                chain.proceed(newRequest.build())
+            }.apply {
+                if (BuildConfig.DEBUG) {
+                    addInterceptor(logging)
+                }
+            }
+            .build()
+    }
 
     @[Provides Singleton]
     fun provideBaseRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
-//            .baseUrl("http://109.123.241.109:46343/") // Real OTP
-            .baseUrl("http://13.60.36.176:8000/") // Test OTP via webhook
-//            .baseUrl("https://ebd8-82-215-105-180.ngrok-free.app/")
-            .client(okHttpClient)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+        .baseUrl("http://13.60.36.176:8000/")
+        .client(okHttpClient)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
 
     @[Provides Singleton Public]
     fun providePublicRetrofit(@Public okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
-//            .baseUrl("http://109.123.241.109:46343/") // Real OTP
-            .baseUrl("http://13.60.36.176:8000/") // Test OTP via webhook
-//            .baseUrl("https://ebd8-82-215-105-180.ngrok-free.app/")
-            .client(okHttpClient)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+        .baseUrl("http://13.60.36.176:8000/")
+        .client(okHttpClient)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
 
 }
